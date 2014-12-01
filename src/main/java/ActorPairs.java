24ae3385178df07a8ee8c8ea4dec3d69f9d1193b
Seth <sjw3kf@mail.missouri.edu>
2014-12-01 16:18:29 -0600

    ActorPairs

---
+++ src/main/java/ActorPairs.java
@@ -0,0 +1,96 @@
+/**
+ * @author  by sethwiesman on 11/25/14.
+ */
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ActorPairs {
+
+    public static class Map extends Mapper<LongWritable, Text, Text, NullWritable> {
+
+        final Pattern pattern;
+
+        public Map() {
+            this.pattern =  Pattern.compile("(.+)\\s\\(\\d{4}\\)(.*)");
+        }
+
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+            final String line = value.toString();
+            final Matcher matcher = pattern.matcher(line);
+
+            if (!matcher.find()) {
+                return;
+            }
+
+            final String[] actors = matcher.group(2).split("/");
+
+            if (actors.length < 2) {
+                return;
+            }
+
+            for (int i = 0; i < actors.length -1 ; ++i) {
+                for (int j = i+1; j < actors.length; ++j) {
+                    StringBuilder builder = new StringBuilder();
+
+                    if (actors[i].compareTo(actors[j]) == 0) {
+                        continue;
+                    } else if (actors[i].compareTo(actors[j]) < 0) {
+                        builder.append(actors[i])
+                                .append(' ')
+                                .append(actors[j]);
+                    } else {
+                        builder.append(actors[j])
+                                .append(' ')
+                                .append(actors[j]);
+                    }
+
+                    context.write(new Text(builder.toString()), NullWritable.get());
+
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+
+        if (args.length < 2) {
+            System.out.println("Error");
+            return;
+        }
+
+        Configuration conf = new Configuration();
+        Job job = new Job(conf, "actor pairs");
+        job.setJarByClass(ActorPairs.class);
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(NullWritable.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(NullWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        boolean result = job.waitForCompletion(true);
+        System.exit(result ? 0 : 1);
+
+    }
+}
+
